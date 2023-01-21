c8603228da397b460811c3272aef5d142dec8571
Justin <mbanefojustin@gmail.com>
2023-01-21 15:07:00 -0600

    updated java

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,134 @@
+// Start button & Info Box
 const start = document.querySelector("#begin")
-const Bye = document.querySelector("#welcome")
-
-const Qt1 = document.querySelector(".option")
-const Title = document.querySelector("#quest")
 const info1 = document.querySelector(".explainBox")
+const welTitle = document.querySelector("#welcome")
 const info2 = document.querySelector("#info")
-const QtBox = document.querySelector(".quizbox")
+// Questions & Quiz box
+const title = document.querySelector("#quest")
+const questionEL1 = document.querySelector("#opt1")
+const questionEL2 = document.querySelector("#opt2")
+const questionEL3 = document.querySelector("#opt3")
+const questionEL4 = document.querySelector("#opt4")
+const quizsection = document.querySelector(".quizbox")
+const disClock = document.querySelector(".secs")
+//Scorebox & Initialize timer
+const record = document.querySelector("#highscore")
+const alphaScore = document.querySelector("#topscore")
+let score = 0
+let timercount = 60
 
 
+const questions = [
+    {
+        question: "How does this even work?",
+        choice1: "I have no idea",
+        choice2: "I will never know",
+        choice3: "I am working to figure that out",
+        choice4: "Give up",
+        answer: 3,
+    },
 
-
-const questions =  [
-    {Question: "How does this even work?", 
-    Choice1: "I have no idea",
-    Choice2: "I will never know",
-    Choice3: "I am working to figure that out", 
-    Choice4: "Gvie up", 
-    Answer: 3},
-
-    {Question: "How does this even work?", 
-    Choice1: "I have no idea",
-    Choice2: "I will never know",
-    Choice3: "I am working to figure that out", 
-    Choice4: "Gvie up",  
-    Answer: 1, 
+    {
+        question: "Did you figure it out?",
+        choice1: "Absolutely",
+        choice2: "I tried",
+        choice3: "Patience is a virture",
+        choice4: "Lost me",
+        answer: 1,
     }
-
-
 ]
+    function clock() {
+        timer = setInterval(function () {
+            console.log(timercount)
+            timercount--;
+            if (timercount >= 0) {
+                disClock.innerHTML = timercount
+                // Then check for win
+            } else {
+                clearInterval(timer)
+                alert("GAME OVER")
+                console.log(score)
+            }}, 1000)
+}
 
-function iStart(){
-    console.log("clicked")
-    start.style.display = "none";
-    Bye.innerHTML = "";
-    info1.style.display = "none"
-    info2.style.display = "none"
+    function iStart() {
+        console.log("clicked")
+        clock();
+        start.style.display = "none";
+        welTitle.innerHTML = "";
+        info1.style.display = "none"
+        info2.style.display = "none"
+        quizsection.style.display = "flex"
 
+    // get the choice you want to put in button Question 1
+    let questionTitle = questions[0].question
+    let choice1 = questions[0].choice1;
+    let choice2 = questions[0].choice2; 
+    let choice3 = questions[0].choice3;
+    let choice4 = questions[0].choice4; 
+    // get the choice you want to put in button Question 2
+    let questionTitle2 = questions[1].question
+    let choice01 = questions[1].choice1;
+    let choice02 = questions[1].choice2; 
+    let choice03 = questions[1].choice3;
+    let choice04 = questions[1].choice4; 
+    // put it in the button you want it in
+    title.innerHTML = questionTitle
+    questionEL1.innerHTML = choice1;
+    questionEL2.innerHTML = choice2; 
+    questionEL3.innerHTML = choice3; 
+    questionEL4.innerHTML = choice4
 
-    Qt1.innerHTML = questions[1]
+        
+    function checkme() {
 
-}    
-   
+        if(choice3){
+        console.log("Good guess")
+        score++; 
+        console.log(score)
+        //    New Question 
+        title.innerHTML = questionTitle2
+        questionEL1.innerHTML = choice01;
+        questionEL2.innerHTML = choice02; 
+        questionEL3.innerHTML = choice03; 
+        questionEL4.innerHTML = choice04; 
+        }
+    
+    
+        else { 
+        console.log("You answered incorrectly")
+        title.innerHTML = questionTitle2
+        //    New Question 
+        questionEL1.innerHTML = choice01;
+        questionEL2.innerHTML = choice02; 
+        questionEL3.innerHTML = choice03; 
+        questionEL4.innerHTML = choice04; 
+    }
+}
 
 
 
     
-    // info1.classList.remove("explainBox")
-    // info2.classList.remove(".info") I chnaged this from Class to ID
-    // QtBox.classList.remove("quizbox")
-    // QtBox
-    // document.getElementById("info").innerHtml = "READY?!?!?"
+questionEL1.addEventListener("click", checkme)
+questionEL2.addEventListener("click", checkme)
+questionEL3.addEventListener("click", checkme)
+questionEL4.addEventListener("click", checkme)
+}
+
+//Need to continue with buttons
+
+//Need Local Storage create new page? 
+function recordKeeper() {
+
+}
+
+// Start button
+start.addEventListener("click", iStart)
+// Other Buttons
 
 
 
 
 
 
-start.addEventListener("click",iStart)
+
